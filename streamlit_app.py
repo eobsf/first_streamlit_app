7b074f083133e279a28ab7a487334e3b384be324
eobsf <112211671+eobsf@users.noreply.github.com>
2022-08-30 18:00:30 -0500

    Update streamlit_app.py
    restructure to run some code only once

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,16 +26,21 @@ streamlit.dataframe(fruits_to_show)
 
 #New Section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    streamlit.error("Please select a fruit to get information.")
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+    #streamlit.text(fruityvice_response) comment out this line
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
-#streamlit.text(fruityvice_response) comment out this line
-
-# Normalize semi-structured JSON data into a flat table
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# Display normalized fruit table
-streamlit.dataframe(fruityvice_normalized)
+    # Normalize semi-structured JSON data into a flat table
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    # Display normalized fruit table
+    streamlit.dataframe(fruityvice_normalized)
+    
+except URLError as e:
+  streamlit.error()
 #don't run anything past here while we trouble shoot
 streamlit.stop()
 
